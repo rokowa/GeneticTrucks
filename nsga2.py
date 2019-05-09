03e7644c32a8e8feacd2afe5b82c226c678aea5d
GriffinBabe <darius.couchard@hotmail.com>
2019-05-09 11:41:14 +0200

    Finished algorithm, now testing

--- nsga2.py
+++ nsga2.py
@@ -74,26 +74,37 @@ dataloader = DataLoader("data_maison_com.txt")
 data = dataloader.data
 data.init_distances()
 best_chromosomes = []
+final_solution = []
 
 
 def main(p, q, iteration):
     if iteration > NBR_ITERATIONS:
-        return
+        return p + q
     else:
         print("---------[Iteration {}]----------".format(iteration))
         solutions = p + q
         F = fast_non_dominated_sort(solutions)
         pplus = []
-        i = 1
-        while len(pplus) + len(F[i]) <= MAX_SOLUTIONS:
+        i = 0
+        while len(pplus) + get_safe_f_size(F, i) <= MAX_SOLUTIONS and F[i]:
             F[i] = crowding_distance_assignment(F[i])
             pplus.extend(F[i])
             i += 1
         sorted(F[i], key=lambda x: x.get_fitness_score())
         pplus.extend(F[i][0:(MAX_SOLUTIONS - len(pplus))])
         qplus = make_new_pop(pplus)
-
         return main(pplus, qplus, iteration+1)
+
+
+def get_safe_f_size(F, index):
+    """ This is a solution simple trick for out problem in the while loop of main function
+        If we are crossing the end of the list, we add a final empty front to F, the while
+        loop will stop (check the second condition of the while loop) """
+    if index < len(F):
+        return len(F[index])
+    else:
+        F.append([])
+        return 0
 
 
 def make_new_pop(pplus):
@@ -102,10 +113,11 @@ def make_new_pop(pplus):
     couple_list = []
 
     new_chromosomes = []
-    for i in len(pplus)/2:
+    for i in range(int(len(pplus)/2)):
         couple = random.sample(pplus, 2)
         couple_list.append(couple)
-        pplus.remove(couple[0], couple[1])
+        pplus.remove(couple[0])
+        pplus.remove(couple[1])
     for couple in couple_list:
         (a, b) = couple[0].cross(couple[1])
         new_chromosomes.append(a)
@@ -139,56 +151,77 @@ def dominate(s1, s2):
 
 def fast_non_dominated_sort(pop):
     """ Sorts the population, the best ones are the less dominated ones"""
-    ranks = [0]*pop_size
-    fronts = [[]]
+    ranks = {}  # Dictionary containing key: chromosome, value: it's rank value
+    dominated = {}  # Dictionary containing key: chromosome, value: chromosomes dominated by the chromosome
+    domination_count = {}  # Dictionary containing key: chromosome, value: number of chromosomes that dominate the key
+                           # chromosome
+    fronts = [[]]  # Fronts, at the first row there are all the chromosomes that are non dominated
+    for index, chromosome in enumerate(pop):
+        # Number of chromosomes that dominate chromosome
+        domination_count[chromosome] = 0
+        ranks[chromosome] = 0
+        dominated[chromosome] = []
+        for chromosome2 in pop:
+            if dominate(chromosome, chromosome2):
+                dominated[chromosome].append(chromosome2)
+            elif dominate(chromosome2, chromosome):
+                domination_count[chromosome] += 1
+        if domination_count[chromosome] == 0:
+            ranks[chromosome] = 1
+            fronts[0].append(chromosome)
 
-    dominated = [[]]*pop_size
-    dom_count = [0]*pop_size
-    
-    for i in range(pop_size):
-        for j in range(pop_size):
-            if dominate(pop[i], pop[j]):
-                dominated[i].append(j)
-            elif dominate(pop[j], pop[i]):
-                dom_count[i] += 1
-        if dom_count[i] == 0:
-            ranks[i] = 0
-            fronts[0].append(i)
-    k = 0
-    while len(fronts[k]) > 0:
-        q = []
-        for i in fronts[k]:
-            for j in range(len(dominated)):
-                dom_count[j] -= 1
-                if dom_count[j] == 0:
-                    ranks[j] = k + 1
-                    q.append(j)
-        k += 1
-        fronts.append(q)
-    
-    f = [[pop[i] for i in f] for f in fronts]
-    return f
+    i = 0
+    # Not sure about this one
+    while i < len(fronts):
+        if not fronts[i]:
+            break
+        temp = []
+        # For each chromosome in front i
+        for chromosome in fronts[i]:
+            # For each chromosome dominated by the chromosome we have on iteration
+            for chromosome2 in dominated[chromosome]:
+                domination_count[chromosome2] -= 1
+                if domination_count[chromosome2] == 0:
+                    ranks[chromosome2] += 1
+                    temp.append(chromosome2)
+        i += 1
+        if temp:
+            if i < len(fronts):
+                fronts[i].extend(temp)
+            else:
+                fronts.append(temp)
+
+    return fronts
 
 
 def crowding_distance_assignment(pop_set):
-    l = len(pop_set)
-    crow_dist = [0]*l
-    obj_values = np.empty(len(pop_set))
-
-    for s in pop_set:
-        obj_values.add(s.get_fitness_score)
-        
-    for i in range(2):
-        sorted_idx = np.argsort(obj_values[:, i])
-        
-        min_value = obj_values[sorted_idx[0], i]
-        max_value = obj_values[sorted_idx[l-1], i]
-        crow_dist[sorted_idx[0]] = np.inf
-        crow_dist[sorted_idx[l-1]] = np.inf
-        for j in range(1, l-2):
-            crow_dist[sorted_idx[j]] += (obj_values[sorted_idx[j-1], i] - obj_values[sorted_idx[j+1], i])/(max_value-min_value)
-    
-    return crow_dist
+    """ Chrowding distance calculation, the pop_set is only a front of the total population
+        @:param a population set (a single front of the population)
+        @:return returns the given front of chromosomes sorted by the crowding distance of each chromosome"""
+    solution_nmbr = len(pop_set)
+    distance = [0.0] * len(pop_set)  # List of float distance, the chromosomes are identified by index in this case
+    for m in range(2):  # We iterate over the two chromosome fitness function values
+        sorted(pop_set, key=lambda x: x.get_fitness_score()[m])  # we sort our pop_set by the m objective value
+        max_value = pop_set[0].get_fitness_score()[m]  # Max fitness score of the current m objective
+        min_value = pop_set[-1].get_fitness_score()[m]  # Min fitness score of the current m objective
+        if (max_value == 0 and min_value == 0) or (max_value == min_value):  # Workaround, the max_value and the min_
+                                                                             # value are equals, so we have to find
+                                                                             # a solution to avoid DivideBy0 exception
+            max_value = np.inf
+            min_value = 0
+        distance[0] = np.inf
+        distance[-1] = np.inf
+        for i in range(1, solution_nmbr - 1):
+            distance[i] += (pop_set[i+1].get_fitness_score()[m] - pop_set[i-1].get_fitness_score()[m]) \
+                           / (max_value - min_value)
+    # We sort our list by the distance values
+    joined_list = [[None, 0.0]]*len(pop_set)
+    for i in range(len(pop_set)):
+        joined_list[i] = [pop_set[i], distance[i]]
+    sorted(joined_list, key=lambda x: x[1])
+    # I know there a better way of doing that but i'm kinda by zip rn
+    final_list = [item[0] for item in joined_list]
+    return final_list
 
 
 def initial_data_creator(nbrpopulation):
@@ -212,4 +245,5 @@ def initial_data_creator(nbrpopulation):
 
 
 initial_population = initial_data_creator(4)
-main(initial_population, [], 1)
+final_solution = main(initial_population, [], 1)
+print(final_solution)
